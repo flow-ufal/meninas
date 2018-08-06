f89673324ec1037bcab9157a98311c744574edd1
lucasuchoa98 <38196129+lucasuchoa98@users.noreply.github.com>
2018-08-05 22:26:01 -0300

    class OpenQUestion and Answer added
    Talvez seja uma boa ideia criarmos um campo nos modelos referente a data de criação de certa pergunta aberta

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+""" é a classe das perguntas abertas"""
+class OpenQuestion(models.Model):
+    name = models.CharField(max_length = 20, help_text="nome da pergunta, para referência")
+    questionText = models.CharField(max_length = 500, help_text="O texto da pergunta")
+
+    def __str__(self):
+        return '{0} ({1})'.format(self.name,self.questionText)
+
+class Answer(models.Model):
+    question = models.ForeignKey("OpenQuestion",
+    related_name = "Answer",
+    on_delete=models.CASCADE)
+    answer = models.CharField("Answer", max_length=500)
+
 # Create your models here.
