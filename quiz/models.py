1705c5d2984f7d64a4d7cb8d8734dd4ad7317e3c
lucasuchoa98 <38196129+lucasuchoa98@users.noreply.github.com>
2018-08-12 12:39:40 -0300

    Update models.py
    pub_date adicionado, função was_published_recently adicionada e class meta adicionada

--- quiz/models.py
+++ quiz/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 """ é a classe das perguntas abertas"""
 class OpenQuestion(models.Model):
     name = models.CharField(max_length = 20, help_text="nome da pergunta, para referência")
     questionText = models.CharField(max_length = 500, help_text="O texto da pergunta")
+    pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return '{0} ({1})'.format(self.name,self.questionText)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Answer(models.Model):
     question = models.ForeignKey("OpenQuestion",
@@ -14,4 +20,7 @@ class Answer(models.Model):
     on_delete=models.CASCADE)
     answer = models.CharField("Answer", max_length=500)
 
-# Create your models here.
+    class Meta:
+         unique_together = [
+            ("question", "answer"),
+         ]
