7b52b31877c097351a697fc1ff40b8c092d8b0a7
lucasuchoa98 <38196129+lucasuchoa98@users.noreply.github.com>
2018-08-21 14:21:45 -0300

    novos imports e novos registros

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import OpenQuestion, Answer
+from .models import OpenQuestion, Answer, Questionario, MultipleOptionQuestion, Choice
 
+admin.site.register(MultipleOptionQuestion)
+admin.site.register(Choice)
+admin.site.register(Questionario)
 admin.site.register(OpenQuestion)
 admin.site.register(Answer)
+
 # Register your models here.
