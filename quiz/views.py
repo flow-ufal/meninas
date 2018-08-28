2a3a4ea1ad4aeb1799c4970a2f635fb15f8fbc88
lucasuchoa98 <38196129+lucasuchoa98@users.noreply.github.com>
2018-08-28 10:29:28 -0300

    Views
    Related to Questionare view list pub

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Questionario
+
+
+def index(request):
+    latest_questionare_list = Questionario.objects.order_by('-pub_date')[:5]
+    context = {'latest_questionare_list': latest_questionare_list}
+    return render(request, 'quiz/index.html', context)
+
