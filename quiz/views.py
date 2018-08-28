3a5c8b6d136bf0aaf8c3f7dbb298afd6eed85c8e
lucasuchoa98 <38196129+lucasuchoa98@users.noreply.github.com>
2018-08-28 10:55:19 -0300

    we wrote some views and talz

--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+
 
 from .models import Questionario
 
@@ -8,3 +11,16 @@ def index(request):
     context = {'latest_questionare_list': latest_questionare_list}
     return render(request, 'quiz/index.html', context)
 
+def detail(request, questionario_id):
+    try:
+        questionario = Questionario.objects.get(pk=questionario_id)
+    except Questionario.DoesNotExist:
+        raise Http404("Questionario does not exist")
+    return render(request, 'quiz/detail.html', {'questionario': questionario})
+
+def results(request, questionario_id):
+    response = "You're looking at the results of questionare %s."
+    return HttpResponse(response % questionario_id)
+
+def vote(request, questionario_id):
+    return HttpResponse("You're voting on question %s." % questionario_id)
