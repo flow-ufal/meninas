882bde083c4ede517c628d7627c052e99ec1a523
MateusMNormande <38102708+MateusMNormande@users.noreply.github.com>
2018-11-22 16:59:26 -0300

    Remove paths desnecessários

--- quiz/urls.py
+++ quiz/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
     # ex: /quiz/5/
     path('<int:multipleoptionquestion_id>/', views.perguntas_dentro_do_questionario, name='depois_questionario'),
     # ex: /quiz/5/results/
-    path('<int:questionario_id>/results/', views.results, name='results'),
-    # ex: /quiz/5/vote/
-    path('<int:questionario_id>/vote/', views.vote, name='vote'),
 ]
