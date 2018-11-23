c0a312936f299b7c42089c2725d152525031055f
MateusMNormande <38102708+MateusMNormande@users.noreply.github.com>
2018-11-23 14:49:05 -0300

    Muda os parâmetros da função path

--- quiz/urls.py
+++ quiz/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     # ex: /quiz/5/
-    path('<int:multipleoptionquestion_id>/', views.perguntas_dentro_do_questionario, name='depois_questionario'),
+    path('<int:questionario_id>/', views.perguntas_dentro_do_questionario, name='depois_questionario'),
     # ex: /quiz/5/results/
 ]
