4568a033c45df0ccdf872c3a3817a51db819167d
uchoa <lucasuchoalg@gmail.com>
2018-11-26 01:34:57 -0300

    agora todos os questioanrios sao mostrados em index

--- quiz/views.py
+++ quiz/views.py
@@ -7,8 +7,8 @@ from .models import Questionario, MultipleOptionQuestion, OpenQuestion
 
 
 def index(request):
-    latest_questionare_list = Questionario.objects.order_by('-pub_date')[:5]
-    context = {'latest_questionare_list': latest_questionare_list}
+    questionare_list = Questionario.objects.all()
+    context = {'questionare_list': questionare_list}
     return render(request, 'quiz/index.html', context)
 
 def perguntas_dentro_do_questionario(request, questionario_id):
@@ -17,4 +17,3 @@ def perguntas_dentro_do_questionario(request, questionario_id):
     context = {"multipleopenquestion_list" : multipleopenquestion_list, "openquestion_list" : openquestion_list}
 
     return render(request,'quiz/depois_do_link.html', context)
-
