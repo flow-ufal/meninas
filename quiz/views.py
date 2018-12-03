e883a87a9434929006391a3fca954b9e50cf7170
Mateus <38102708+MateusMNormande@users.noreply.github.com>
2018-12-03 10:32:43 -0300

    Adiciona a lista de escolhas
    É capaz de estar errado :disappointed:

--- quiz/views.py
+++ quiz/views.py
@@ -13,7 +13,10 @@ def index(request):
 
 def perguntas_dentro_do_questionario(request, questionario_id):
     multipleopenquestion_list = MultipleOptionQuestion.objects.filter(delphis = questionario_id)
+    choice_list = MultipleOptionQuestion.objects.filter(choices = questionario_id)
     openquestion_list = OpenQuestion.objects.filter(di = questionario_id)
-    context = {"multipleopenquestion_list" : multipleopenquestion_list, "openquestion_list" : openquestion_list}
+    context = {"multipleopenquestion_list" : multipleopenquestion_list,
+                "openquestion_list" : openquestion_list,
+                "choice_list" : choice_list}
 
     return render(request,'quiz/depois_do_link.html', context)
