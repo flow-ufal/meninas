9283ed25226449e71fb910c57f64a2d37759de1d
lucasuchoa98 <38196129+lucasuchoa98@users.noreply.github.com>
2018-12-04 10:52:56 -0300

    muda o funcionamento do choice
    agora a views passa uma list, não mais um dict

--- quiz/views.py
+++ quiz/views.py
@@ -14,11 +14,11 @@ def index(request):
 def perguntas_dentro_do_questionario(request, questionario_id):
     multipleopenquestion_list = MultipleOptionQuestion.objects.filter(delphis = questionario_id)
     openquestion_list = OpenQuestion.objects.filter(di = questionario_id)
-    choice_dict = {}
-    for multipleoptionquestion in multipleopenquestion_list:
-        choice_dict[multipleoptionquestion.id] = list(Choice.objects.filter(question = multipleoptionquestion.id))
+    choice_list = Choice.objects.filter(question = questionario_id)
+    #for multipleoptionquestion in multipleopenquestion_list:
+    #    choice_dict[multipleoptionquestion.id] = list(Choice.objects.filter(question = multipleoptionquestion_id))
     context = {"multipleopenquestion_list" : multipleopenquestion_list,
                "openquestion_list" : openquestion_list,
-               "choice_dict" : choice_dict}
+               "choice_list" : choice_list}
 
     return render(request,'quiz/depois_do_link.html', context)
